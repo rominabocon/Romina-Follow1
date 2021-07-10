89fafb31be9bb5f615fb333aed3448c7b507eeb5
Romina Bocon <rominabocon@gmail.com>
2021-07-09 19:38:36 -0700

    prueba mail

--- pages/email.php
+++ pages/email.php
@@ -1,60 +1,16 @@
 <?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-//Load Composer's autoloader
-require 'vendor/autoload.php';
-
-//Instantiation and passing `true` enables exceptions
-$mail = new PHPMailer(true);
-
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'rominabocon@gmail.com';                     //SMTP username
-    $mail->Password   = 'Patronus2020!';                         //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-    //Recipients
-    $mail->setFrom('rominabocon@gmail.com', 'Romina Bocon'); // Hacer coincidir con el username. (preferentemente)
-    $mail->addAddress('rominabocon@gmail.com', 'Romina Bocon');     //Add a recipient
-    //$mail->addAddress('ellen@example.com');               //Name is optional
-    //$mail->addReplyTo('info@example.com', 'Information');
-    //$mail->addCC('villegasmatias.asdf@gmail.com');
-    //$mail->addBCC('bcc@example.com');
-
-    //Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Consulta de: '.$_POST['inp_nombre'];
-    $mail->Body    = 
-    'Nombre: '.$_POST['inp_nombre'].
-    '<br>Email: '.
-    $_POST['inp_mail'].
-    '<br>Adultos: '.
-    $_POST['inp_adultos'].
-    '<br>Niños: '.$_POST['inp_ninos'];
-    '<br>Parques: '.
-    $_POST['inp_parques'].
-    '<br>Comentarios: '.
-    $_POST['inp_coment'].
-    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    header("location: gracias.html");
-} catch (Exception $e) {
-    //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    header("location: error.html");
-}
-
-?>
+$email_to = "rominabocon@gmail.com";
+$email_subject = "Contacto desde la web";
+$email_message = "Detalles del formulario de contacto:\n\n";
+$email_message .= "Nombre: " . $_POST['inp_nombre'] . "\n";
+$email_message .= "E-mail: " . $_POST['inp_mail'] . "\n";
+$email_message .= "Cantidad de adultos: " . $_POST['inp_adultos'] . "\n";
+$email_message .= "Cantidad de niños: " . $_POST['inp_ninos'] . "\n";
+$email_message .= "Parques: " . $_POST['inp_parques'] . "\n";
+$email_message .= "Comentarios: " . $_POST['inp_coment'] . "\n\n";
+$headers = 'From: ' . $email_to . "\r\n" . 'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+echo '<script type="text/javascript">
+		    	alert("Gracias por su consulta, me comunicaré a la brevedad");
+		    	window.location.href="/index.html";
+		      </script>';
