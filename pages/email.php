b3d9d95e0ec05d5e1a02c1d5621c50357fa3e62f
Romina Bocon <rominabocon@gmail.com>
2021-07-12 15:44:12 -0700

    correcciones formulario

--- pages/email.php
+++ pages/email.php
@@ -3,6 +3,7 @@ $email_to = "rominabocon@gmail.com";
 $email_subject = "Contacto desde la web";
 $email_message = "Detalles del formulario de contacto:\n\n";
 $email_message .= "Nombre: " . $_POST['inp_nombre'] . "\n";
+$email_message .= "Telefono: " . $_POST['inp_tel'] . "\n";
 $email_message .= "E-mail: " . $_POST['inp_mail'] . "\n";
 $email_message .= "Cantidad de adultos: " . $_POST['inp_adultos'] . "\n";
 $email_message .= "Cantidad de niños: " . $_POST['inp_ninos'] . "\n";
@@ -11,6 +12,6 @@ $email_message .= "Comentarios: " . $_POST['inp_coment'] . "\n\n";
 $headers = 'From: ' . $email_to . "\r\n" . 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);
 echo '<script type="text/javascript">
-		    	alert("Gracias por su consulta, me comunicaré a la brevedad");
+		    	alert("Gracias por su consulta, me comunicaré dentro de las proximas 48hs para comenzar a programar sus proximas vacaciones mágicas.");
 		    	window.location.href="/index.html";
 		      </script>';
